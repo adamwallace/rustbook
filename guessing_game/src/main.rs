5ae8bd0d815f44ede7d21044d9b14fdd787e34d7
Adam Wallace <adam.wallace1995@gmail.com>
2021-10-10 13:42:19 -0500

    fix file structure

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+
+fn main() {
+    println!("guess the number");
+    println!("enter your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guessed {}", guess);
+
+    // TODO: finish
+}
